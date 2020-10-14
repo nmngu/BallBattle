af94269a2dfd9164ee84468b1935d5a05405e038
ngu.nguyenminh <nmngu91@gmail.com>
2020-10-14 17:06:48 +0700

    - Create energy bar and timer.

---
+++ Assets/Scripts/EnergyFiller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class EnergyFiller : MonoBehaviour
+{
+
+    private int energy = 0;
+    private int mintuesBetweenFills = 5;
+    private DateTime lastEnergyFill;
+    float enemy_fill_amount = 0;
+    float player_fill_amount = 0;
+    public GameObject enemy_energy;
+    public GameObject player_energy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastEnergyFill = DateTime.Parse(PlayerPrefs.GetString("lastEnergyFill", DateTime.Now.ToString()));
+    }
+    private void FixedUpdate()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        TimeSpan timeSinceEnergyFill = (DateTime.Now - lastEnergyFill);
+        if (timeSinceEnergyFill > TimeSpan.FromMinutes(mintuesBetweenFills))
+        {
+            int ammount = timeSinceEnergyFill.Minutes / mintuesBetweenFills;
+            energy += ammount;
+            lastEnergyFill -= TimeSpan.FromMinutes(mintuesBetweenFills * ammount);
+            PlayerPrefs.SetString("lastEnergyFill", lastEnergyFill.ToString());
+        }
+
+        enemy_energy.GetComponent<UnityEngine.UI.Image>().fillAmount = enemy_fill_amount;
+        player_energy.GetComponent<UnityEngine.UI.Image>().fillAmount = player_fill_amount;
+    }
+
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,6 +11,8 @@ public class GameController : MonoBehaviour
     private List<GameObject> list_players;
     private List<GameObject> list_enemies;
     private Vector3 ballPosition;
+    private float match_time = 140;
+    float m_time = 0;
     void Start()
     {
         ballPosition = new Vector3(Random.Range(0.0f, -6.0f), 0.3f, Random.Range(-4.0f, 4.0f));// y = 0.3 to place ball on plane
@@ -20,6 +22,11 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        m_time += Time.deltaTime;
+        // Debug.Log("tIME " + m_time);
+        GameObject remain_time = GameObject.FindGameObjectWithTag("RemainTime");
+        int i_remainTime = (int)(match_time - m_time);
+        remain_time.GetComponent<UnityEngine.UI.Text>().text = i_remainTime.ToString() + "s";
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
